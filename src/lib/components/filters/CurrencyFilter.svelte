1c6a2ff1197fdc207cd48359aa8781d62f3875fa
YZhLu <luiz.zito@icloud.com>
2024-11-26 17:42:32 -0300

    Create CurrencyFilter.svelte

---
+++ src/lib/components/filters/CurrencyFilter.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import {
+		Autocomplete,
+		popup,
+		type AutocompleteOption,
+		type PopupSettings
+	} from '@skeletonlabs/skeleton';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	type CurrencyOption = AutocompleteOption<string>;
+
+	let inputPopupDemo = '';
+	let popupSettings: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupAutocomplete',
+		placement: 'bottom'
+	};
+
+	export let options: string[];
+	export let currencyOptions: CurrencyOption[] = options?.map((o: string) => {
+		return { label: o, value: o };
+	});
+
+	function onPopupDemoSelect(event: CustomEvent<CurrencyOption>): void {
+		inputPopupDemo = event.detail.label;
+		dispatch('currency-selected', {
+			currency: inputPopupDemo
+		});
+	}
+</script>
+
+<input
+	class="input autocomplete w-48"
+	type="search"
+	name="autocomplete-search"
+	bind:value={inputPopupDemo}
+	placeholder="Select currency..."
+	use:popup={popupSettings}
+/>
+
+<div data-popup="popupAutocomplete" class="card w-48 shadow-xl py-2 ">
+	<div class="arrow bg-surface-800-100-token" />
+	<div class="h-40 overflow-y-auto">
+		<Autocomplete
+			bind:input={inputPopupDemo}
+			options={currencyOptions}
+			on:selection={onPopupDemoSelect}
+		/>
+	</div>
+</div>
