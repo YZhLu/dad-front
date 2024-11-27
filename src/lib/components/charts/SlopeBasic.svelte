2188b6881e7e1fcb60333c6ce491d6f012baa524
YZhLu <luiz.zito@icloud.com>
2024-11-27 14:33:04 -0300

    Update SlopeBasic.svelte
    Add default value and fix sizes

--- src/lib/components/charts/SlopeBasic.svelte
+++ src/lib/components/charts/SlopeBasic.svelte
@@ -4,19 +4,62 @@
 
 	let _options: ApexOptions = {
 		chart: {
-          height: 350,
-          width: 400,
-          type: 'line',
-        },
-        plotOptions: {
-          line: {
-            isSlopeChart: true,
-          },
-        }
+			height: '100%',
+			width: '100%',
+			type: 'line'
+		},
+		plotOptions: {
+			line: {
+				isSlopeChart: true
+			}
+		}
 	};
 
-	export let series: ApexAxisChartSeries;
-	export let title: ApexTitleSubtitle = {};
+	export let series: ApexAxisChartSeries = [
+		{
+			name: 'Blue',
+			data: [
+				{
+					x: 'Jan',
+					y: 43
+				},
+				{
+					x: 'Feb',
+					y: 58
+				}
+			]
+		},
+		{
+			name: 'Green',
+			data: [
+				{
+					x: 'Jan',
+					y: 33
+				},
+				{
+					x: 'Feb',
+					y: 38
+				}
+			]
+		},
+		{
+			name: 'Red',
+			data: [
+				{
+					x: 'Jan',
+					y: 55
+				},
+				{
+					x: 'Feb',
+					y: 21
+				}
+			]
+		}
+	];
+	export let title: ApexTitleSubtitle = {
+		text: 'TEST DATA',
+		align: 'left'
+	};
 	export let options = _options;
 
 	const chart: Chart = {
